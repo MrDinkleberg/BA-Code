75eded650b8660bea1b873325b60a33e70f7f6d2
lars <lars.bindert@gmail.com>
2020-06-08 16:52:42 +0200

    added test class

---
+++ src/TestMain.java
@@ -0,0 +1,11 @@
+public class TestMain {
+
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+
+        MemoryManager memoryManager = new MemoryManager(1000000000);
+
+
+
+    }
+
+}
