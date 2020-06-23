eae8374fcfa5053421a7fd317861f60399c4e10e
lars <lars.bindert@gmail.com>
2020-06-23 17:49:08 +0200

    added reading and writing methods for other primitives

--- src/MemoryManager.java
+++ src/MemoryManager.java
@@ -10,10 +10,65 @@ public class MemoryManager {
         return offHeap.writeInt(value);
     }
 
+    public long writeLong(long value){
+        return offHeap.writeLong(value);
+    }
+
+    public long writeDouble(double value){
+        return offHeap.writeDouble(value);
+    }
+
+    public long writeChar(char value){
+        return offHeap.writeChar(value);
+    }
+
+    public long writeByte(byte value){
+        return offHeap.writeByte(value);
+    }
+
+    public long writeShort(short value){
+        return offHeap.writeShort(value);
+    }
+
+    public long writeFloat(float value){
+        return offHeap.writeFloat(value);
+    }
+
+    public long writeBoolean(boolean value){
+        return offHeap.writeBoolean(value);
+    }
+
     public int readInt(long address){
         return offHeap.readInt(address);
     }
 
+    public long readLong(long address){
+        return offHeap.readLong(address);
+    }
+
+    public double readDouble(long address){
+        return offHeap.readDouble(address);
+    }
+
+    public char readChar(long address){
+        return offHeap.readChar(address);
+    }
+
+    public byte readByte(long address){
+        return offHeap.readByte(address);
+    }
+
+    public short readShort(long address){
+        return offHeap.readShort(address);
+    }
+
+    public float readFloat(long address){
+        return offHeap.readFloat(address);
+    }
+
+    public boolean readBoolean(long address){
+        return offHeap.readBoolean(address);
+    }
 
     public void cleanup(){
         offHeap.freeMemory(offHeap.startaddress);
--- src/OffHeap.java
+++ src/OffHeap.java
@@ -35,14 +35,88 @@ public class OffHeap {
         unsafe.putInt(nextaddress, value);
         long address = nextaddress;
         nextaddress += Integer.BYTES;
+        return address;
+    }
 
+    public long writeLong(long value){
+        unsafe.putLong(nextaddress, value);
+        long address = nextaddress;
+        nextaddress += Long.BYTES;
+        return address;
+    }
+
+    public long writeDouble(double value){
+        unsafe.putDouble(nextaddress, value);
+        long address = nextaddress;
+        nextaddress += Double.BYTES;
+        return address;
+    }
+
+    public long writeChar(char value){
+        unsafe.putChar(nextaddress, value);
+        long address = nextaddress;
+        nextaddress += Character.BYTES;
+        return address;
+    }
+
+    public long writeByte(byte value){
+        unsafe.putByte(nextaddress, value);
+        long address = nextaddress;
+        nextaddress += Byte.BYTES;
+        return address;
+    }
+
+    public long writeShort(short value){
+        unsafe.putShort(nextaddress, value);
+        long address = nextaddress;
+        nextaddress += Short.BYTES;
+        return address;
+    }
+
+    public long writeFloat(float value){
+        unsafe.putFloat(nextaddress, value);
+        long address = nextaddress;
+        nextaddress += Float.BYTES;
+        return address;
+    }
+
+    public long writeBoolean(boolean value){
+        unsafe.putBoolean(null, nextaddress, value);
+        long address = nextaddress;
+        nextaddress += 1;
         return address;
     }
 
     public int readInt(long address){
-        return unsafe.getInt(null, address);
+        return unsafe.getInt(address);
     }
 
+    public long readLong(long address){
+        return unsafe.getLong(address);
+    }
 
+    public double readDouble(long address){
+        return unsafe.getDouble(address);
+    }
+
+    public char readChar(long address){
+        return unsafe.getChar(address);
+    }
+
+    public byte readByte(long address){
+        return unsafe.getByte(address);
+    }
+
+    public short readShort(long address){
+        return unsafe.getShort(address);
+    }
+
+    public float readFloat(long address){
+        return unsafe.getFloat(address);
+    }
+
+    public boolean readBoolean(long address){
+        return unsafe.getBoolean(null, address);
+    }
 
 }
